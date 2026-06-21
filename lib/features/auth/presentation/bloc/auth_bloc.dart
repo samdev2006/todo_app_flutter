31ca49335e6d2f34103061d11ee7e5d92b31db69
samuel <samueladouko23@gmail.com>
2026-06-21 17:32:19 +0000

    auth: création des méthodes dans le auth_bloc;

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -8,13 +8,39 @@ import 'package:todoapp/features/auth/domain/userEntity.dart';
 part 'auth_event.dart';
 part 'auth_state.dart';
 
-class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial());
+class AuthBloc extends Bloc<AuthEvent,AuthState>{
+  final SignInUseCase signInUseCase;
+  final SignUpUseCase signUpUseCase;
+  final SignOutUseCase signOutUseCase;
 
-  @override
-  Stream<AuthState> mapEventToState(
-    AuthEvent event,
-  ) async* {
-    // TODO: implement mapEventToState
+  AuthBloc({
+    required this.signInUseCase,
+    required this.signOutUseCase,
+    required this.signUpUseCase,
+  }) : super(AuthInitial()){
+    on<SignInRequested>((event,emit) async {
+      emit(AuthLoading());
+      try{
+        final user = await signInUseCase(event.email, event.password);
+        emit(AuthSuccess(user));
+      } catch(e){
+        emit(AuthFailure(e.toString()));
+      }
+    });
+
+    on<SignInRequested>((event,emit) async {
+      emit(AuthLoading());
+      try{
+        final user = await signUpUseCase(event.email,event.password);
+        emit(AuthSuccess(user));
+      }catch(e) {
+        emit(AuthFailure(e.toString()));
+      }
+    });
+    on<SignOutRequested>((event,emit) async {
+      await signOutUseCase();
+      emit(AuthInitial());
+    });
   }
+
 }
